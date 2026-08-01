6b3534f24a2e0cd69d7b1141bd00af2028fc9414
HieuP0 <hieu.01227502387@gmail.com>
2026-08-01 17:11:39 +0700

    Router

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
     res.send("Hello world " + (nums++))
 })
 
+app.get('/blog', (req, res) => {
+    res.send("Blog Page")
+})
+
+app.get('/contact', (req, res) => {
+    res.send("Contact Page")
+})
+
 app.listen(3000, () => {
     console.log("server started")
 })
